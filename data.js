29a7a57a0cfb1d6a002855c367896255b41796a2
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-07-14 12:56:25 +0100

    navbar & sidebar update

---
+++ data.js
@@ -0,0 +1,26 @@
+export const bookData = [
+    {
+        "id": "1",
+        "title": "The Effective Engineer",
+        "logo": require("./assets/images/effective engineer.png"),
+        "author": "Edmond Lau",
+        "status": "Available",
+        "year": 2009,
+        "genre": "Productivity",
+        "ratings": 4.0,
+        "users": 31,
+        "likes": 29
+    },
+    {
+        "id": "2",
+        "title": "Built To Last",
+        "logo": require("./assets/images/built to last.svg"),
+        "author": "Edmond Lau",
+        "status": "Borrowed Out",
+        "year": 2001,
+        "genre": "Business, Entrepreneurship",
+        "ratings": 3.0,
+        "users": 22,
+        "likes": 15
+    }
+]
