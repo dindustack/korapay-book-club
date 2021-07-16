55de3bcecf381361cf117073179b2a999f01d346
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-07-16 23:54:05 +0100

    all books fix

--- assets/js/books.js
+++ assets/js/books.js
@@ -12,6 +12,69 @@ async function getBooks() {
 }
 
 // Load Books
+const allBookCover = document.querySelector(".all-cover");
+
+window.addEventListener("DOMContentLoaded", async function () {
+  const books = await getBooks();
+  displayAllBookItems(books);
+});
+
+// Display recently added book items
+const displayAllBookItems = (items) => {
+  let displayAllBook = items.map(
+    (book) => ` 
+      
+      <div class="book-card">
+        <img src=${book.logo} alt="" class="book-card-image">
+        <div class="book-card-body">
+            <span class="h6 ${getBookStatus(book.status)}" id="book-status">${
+      book.status
+    }</span>
+            <h5 class="book-card-title mt-2">${book.title}</h5>
+            <span class="h6">${book.author} - ${book.year}</span>
+            <h6>${book.genre}</h6>
+            <div class="book-card-stats">
+                <div class="ratings">
+                  <span class="h6">Ratings: 4.0</span>
+                  <div>
+                    <img src="./assets/images/star-full.svg" alt="">
+                    <img src="./assets/images/star-full.svg" alt="">
+                    <img src="./assets/images/star-full.svg" alt="">
+                    <img src="./assets/images/star-full.svg" alt="">
+                    <img src="./assets/images/star-empty.svg" alt="">
+                  </div>
+                </div>
+                <div class="book-likes mr-2"></div>
+                <div class="mr-2">
+                <h6 class="mb-0"><img src="./assets/images/user.svg" alt=""></h6>
+                <span class="h6">${book.users}</span>
+
+                </div>
+                <div>
+                <h6 class="mb-0"><img src="./assets/images/love.svg" alt=""></h6>
+                <span class="h6">${book.likes}</span>
+
+                </div>
+                
+            </div>
+        </div>
+      </div>
+      `
+  );
+
+  displayAllBook = displayAllBook.join("");
+  if (allBookCover) {
+    class AllBookCard extends HTMLElement {
+      connectedCallback() {
+        allBookCover.innerHTML = displayAllBook;
+      }
+    }
+
+    customElements.define('all-card', AllBookCard) 
+  } 
+};
+
+
 const bookCover = document.querySelector(".book-cover");
 
 window.addEventListener("DOMContentLoaded", async function () {
@@ -19,8 +82,7 @@ window.addEventListener("DOMContentLoaded", async function () {
   displayBookItems(books);
 });
 
-
-// Dispaly book items
+// Display book items
 const displayBookItems = (items) => {
   let displayBook = items.map(
     (book) => ` 
@@ -28,7 +90,9 @@ const displayBookItems = (items) => {
       <div class="book-card">
         <img src=${book.logo} alt="" class="book-card-image">
         <div class="book-card-body">
-            <span class="h6 ${getBookStatus(book.status)}" id="book-status">${book.status}</span>
+            <span class="h6 ${getBookStatus(book.status)}" id="book-status">${
+      book.status
+    }</span>
             <h5 class="book-card-title mt-2">${book.title}</h5>
             <span class="h6">${book.author} - ${book.year}</span>
             <h6>${book.genre}</h6>
@@ -63,11 +127,17 @@ const displayBookItems = (items) => {
 
   displayBook = displayBook.join("");
   if (bookCover) {
-    bookCover.innerHTML = displayBook;
-  }
+    class BookCard extends HTMLElement {
+      connectedCallback() {
+        bookCover.innerHTML = displayBook;
+      }
+    }
+
+    customElements.define('book-card', BookCard) 
+  } 
 };
 
-// Change text-color of book status 
+// Change text-color of book status
 function getBookStatus(status) {
   if (status === "Available") {
     return "text-green";
