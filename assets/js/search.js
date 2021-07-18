43a7d93aeebb49c3777d5774abb338ee25b772f0
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-07-19 00:09:20 +0100

    filter books fix

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,14 @@
 const searchInput = document.querySelector(".form-control");
 const suggestions = document.querySelector(".search-results");
+const recAddSection = document.querySelector(".book-cover");
+const allBookSection = document.querySelector(".all-cover");
 const mobileSearchInput = document.querySelector(".mobile-form-control");
 const mobileSuggestions = document.querySelector(".mobile-search-results");
 const searchIcon = document.querySelector(".search-icon");
 const mobileSearchIcon = document.querySelector(".mobile-search-icon");
 const mobileNavbar = document.querySelector(".mobile-navbar");
 const headerArrow = document.querySelector(".header-arrow");
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
 const everyBookCover = document.querySelector(".all-cover");
 const addBookCover = document.querySelector(".book-cover");
 
@@ -31,6 +33,7 @@ function numberWithCommas(x) {
 // Display autocomplete character match for the book's title or author
 function displayMatches() {
   const matchArray = findMatches(this.value, books);
+  const filterBookSections = findMatches(this.value, books);
   const html = matchArray
     .map((book) => {
       const regex = new RegExp(this.value, "gi");
@@ -52,6 +55,51 @@ function displayMatches() {
     .join("");
   suggestions.innerHTML = html;
   mobileSuggestions.innerHTML = html;
+
+  // Filter out book items based on text in search box
+  const filteredBookSection = filterBookSections
+    .map(
+      (book) => `  
+  <div class="book-card">
+    <img src=${book.logo} alt="" class="book-card-image">
+    <div class="book-card-body">
+        <span class="h6 ${getBookStatus(book.status)}" id="book-status">${
+        book.status
+      }</span>
+        <h5 class="book-card-title mt-2">${book.title}</h5>
+        <span class="h6">${book.author} - ${book.year}</span>
+        <h6>${book.genre}</h6>
+        <div class="book-card-stats">
+            <div class="ratings">
+              <span class="h6">Ratings: 4.0</span>
+              <div>
+                <img src="./assets/images/star-full.svg" alt="">
+                <img src="./assets/images/star-full.svg" alt="">
+                <img src="./assets/images/star-full.svg" alt="">
+                <img src="./assets/images/star-full.svg" alt="">
+                <img src="./assets/images/star-empty.svg" alt="">
+              </div>
+            </div>
+            <div class="book-likes mr-2"></div>
+            <div class="mr-2">
+            <h6 class="mb-0"><img src="./assets/images/user.svg" alt=""></h6>
+            <span class="h6">${book.users}</span>
+
+            </div>
+            <div>
+            <h6 class="mb-0"><img src="./assets/images/love.svg" alt=""></h6>
+            <span class="h6">${book.likes}</span>
+
+            </div>
+            
+        </div>
+    </div>
+  </div>
+  `
+    )
+    .join("");
+  recAddSection.innerHTML = filteredBookSection;
+  allBookSection.innerHTML = filteredBookSection;
 }
 
 // Event listeners for the input box (desktop view)
@@ -88,4 +136,5 @@ mobileSearchIcon.addEventListener("click", () => {
 // Close search input
 headerArrow.addEventListener("click", () => {
   mobileNavbar.classList.add("d-none");
-  navbar.classList.remove("d-none");});
+  navbar.classList.remove("d-none");
+});
